372e79afcb44287a2f07e8e77a3042b3aa0eb498
Eromer55 <85547094+Eromer55@users.noreply.github.com>
2022-05-30 20:41:45 +0300

    Update and rename ._rfm.py to rfm_analysis.py

--- rfm_analysis.py
+++ rfm_analysis.py
@@ -1,28 +1,3 @@
-##############################################################################################
-# RFM ile Müşteri Segmentasyonu
-###############################################################################################
-
-##############################################################################################
-# 1.İş Problemi
-###############################################################################################
-
-#Bir e-ticaret şirketi müşterilerini segmentlere ayırıp bu segmentlere göre pazarlama stratejileri belirlemek istiyor.
-
-#Değişkenler
-
-#İnvoiceNo : Fatura numarası : Her işleme yani faturaya eşsiz numara. C ile başlıyorsa iptal edilen işlem.
-# StockCode : Ürün kodu. Her bir ürün için eşsiz numara.
-# Description : Ürün ismi
-# Quantity : Ürün adedi. Faturalardaki ürünlerden kaçar tane satıldığını ifade etmektedir.
-# UnitPrice :  Ürün fiyatı (Sterlin üzerinden)
-# InvoiceDate :  Fatura tarihi ve zamanı
-# CustomerId : Eşsiz Müşteri numarası
-# Country : Ülke ismi : Müşterinin yaşadığı ülke
-
-##############################################################################################
-# 2.Veriyi Anlama
-###############################################################################################
-
 import datetime as dt
 import pandas as pd
 pd.set_option('display.max_columns', None)
